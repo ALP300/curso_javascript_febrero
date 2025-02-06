0477c8c21c88914e9300a37597ac0d1a62b94495
ALP300 <aitorpalomino1@gmail.com>
2025-02-05 21:44:48 -0500

    Subir variables

---
+++ ejercicio1.js
@@ -0,0 +1,16 @@
+let num1= parseInt(prompt("Ingresa el número 1: "));
+let num2= parseInt(prompt("Ingresa el número 2: "));
+let num4= parseFloat(prompt("Ingresa el número 2: "));
+const num3= 7;
+let suma= num1+num2
+let resta= num1-num2
+let mult= num1*num2
+let div= num1/num2
+//var num3=4;
+//7
+console.log("EL número 1 es: "+num1)
+console.log("El número 2 es: "+ num2);
+console.log("La suma es: "+suma)
+console.log("La resta es: "+resta)
+console.log("La multiplicación es: "+mult)
+console.log("La división es: "+div)
