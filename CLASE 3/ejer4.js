a41201e3114a17779164f6b68b54c8e3a589b28c
ALP300 <aitorpalomino1@gmail.com>
2025-02-10 20:43:51 -0500

    Realizar ejercicio 4

---
+++ CLASE 3/ejer4.js
@@ -0,0 +1,15 @@
+/*
+Comparación de dos números:
+Declara dos variables a y b con valores numéricos y utiliza condicionales para
+comparar si a es mayor, menor o igual a b.
+*/
+
+let a=7;
+let b=7;
+if(a>b){
+    console.log("A es mayor a B")
+}else if(a<b){
+    console.log("A es menor a B")
+}else{
+    console.log("A es igual a B")
+}
